28c8b67632aebc998f80684ee12d3537d1cac789
Verdi R-D <verdi@azend.org>
2014-12-11 18:58:19 -0500

    Added scrolling text opening scene and opendive dependency

---
+++ SpeedBikeRacer/Assets/OpenScrollingText/PlatformConfigurator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformConfigurator : MonoBehaviour {
+
+	public GameObject standardCamera;
+	public GameObject diveCamera;
+
+	// Use this for initialization
+	void Start () {
+#if UNITY_ANDROID
+		ConfigureForDive();
+#else
+		ConfigureForStandard();
+#endif
+	}
+
+	private void ConfigureForStandard() {
+		DisableCamera (diveCamera);
+
+	}
+	
+	private void ConfigureForDive() {
+		DisableCamera (standardCamera);
+	}
+
+	private void DisableCamera(GameObject cameraObject) {
+		Camera[] cameras = cameraObject.GetComponentsInChildren<Camera> ();
+		foreach (Camera camera in cameras) {
+			Debug.Log(camera.name);
+			camera.enabled = false;
+		}
+	}
+}
---
+++ SpeedBikeRacer/Assets/OpenScrollingText/TextScroller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextScroller : MonoBehaviour {
+
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.localPosition += new Vector3 (0, 1 * speed * Time.deltaTime, 0);
+	}
+}
