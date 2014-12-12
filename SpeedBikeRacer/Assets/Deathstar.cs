ab81aeb86df0d5bc28c84ba8eb5405b5b5ec360a
MathewCain <goaliesupreme@hotmail.com>
2014-12-12 02:28:35 -0500

    yep

---
+++ SpeedBikeRacer/Assets/Deathstar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Deathstar : MonoBehaviour {
+    int y = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            y = -15;
+            rigidbody.MovePosition(new Vector3(-20, y, -5));
+        }
+	}
+}
