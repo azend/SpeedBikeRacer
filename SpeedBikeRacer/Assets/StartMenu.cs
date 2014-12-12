2267c6ada1b1097560171d3d3af0b1f0fba43605
MathewCain <goaliesupreme@hotmail.com>
2014-12-11 20:56:42 -0500

    Here it is

---
+++ SpeedBikeRacer/Assets/StartMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+    void FixedUpdate()
+    {
+        if (Time.timeScale != 0)
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
